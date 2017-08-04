5a29ea13c6061eb205b78d220e9a3f0f3e677bce
Elon Gaffin-Cahn <egaffincahn@gmail.com>
2017-08-03 20:34:05 -0400

    Updated plotgraph
    Now it plots a bunch of info in a figure window, updating every time a
    player puts a new piece down.

--- plotgraph.m
+++ plotgraph.m
@@ -1,63 +1,70 @@
 function plotgraph(turn, G, cards, info)
-% return
-%% plots player 1's goals and locations
-while 0
-    h = plot(G.distance, 'NodeColor', [.75 .75 .75], 'EdgeColor', [.75 .75 .75]);
-    h.EdgeLabel = G.distance.Edges.Weight;
-    
-    player = 1;
-    colors = jet(length(cards.playergoals{player}));
+
+% num discards 
+% x,y positions of cities
+% goal completions
+% pieces remaining
+
+clf
+playercolors = jet(info.players);
+
+playerspots = [... % subplot indices
+    5 6; ...
+    30 40; ...
+    90 100; ...
+    81 91; ...
+    21 31];
+
+longestroadpoints = addlongestroad(G, info);
+for player = 1:info.players
+    subplot(12,10,playerspots(player,:))
+    yspread = .25; if player == 1; yspread = yspread * 2; end
+    ypos = 1.5;            text(0, ypos, sprintf('Player %d', player), 'FontWeight', 'bold', 'Color', playercolors(player,:))
+    ypos = ypos - yspread; text(0, 1.5-yspread*1, sprintf('%d train cars remaining', info.pieces(player)))
+    ypos = ypos - yspread; text(0, 1.5-yspread*2, sprintf('%d cards in hand', length(cards.hand{player})))
+    ypos = ypos - yspread;
+    yspread = yspread / 1.5;
     for goal = 1:length(cards.playergoals{player})
-        highlight(h, cards.playergoals{player}{goal}{1}, 'NodeColor', colors(goal,:), 'MarkerSize', 10)
-        highlight(h, cards.playergoals{player}{goal}{2}, 'NodeColor', colors(goal,:), 'MarkerSize', 10)
+        text(0, ypos, sprintf('%s - %s', cards.playergoals{player}{goal}{1}, cards.playergoals{player}{goal}{2}))
+        ypos = ypos - yspread;
     end
-    for edge = 1:numedges(G.taken)
-        player = G.taken.Edges.Weight(edge);
-        if player == 1
-            [s, t] = findedge(G.taken, edge);
-            highlight(h, s, t, 'EdgeColor', 'k', 'LineWidth', 5)
-        end
+    if player == turn
+        axis off
+        text(-.5, 1.5-yspread*0, '*', 'FontWeight', 'bold', 'FontSize', 36)
+    else
+        axis off
     end
 end
 
-%% plots all taken
+subplot(12,10,101:120)
+text(0, 1, sprintf('%d cards in deck', length(cards.deck)), 'FontSize', 14)
+text(0, .75, [sprintf('%d ', info.points) 'track points'])
+text(0, .50, [sprintf('%d ', longestroadpoints) 'longest points'])
+text(0, .25, [sprintf('%d ', info.points+longestroadpoints) 'total points'], 'FontSize', 14)
+axis off
+
+%% plot graph
+subplot(12,10,intersect(11:100, find(ismember(mod(1:120, 10), 2:9))))
 h = plot(G.taken, 'NodeColor', [.75 .75 .75], 'EdgeColor', [.75 .75 .75]);
 h.EdgeLabel = G.distance.Edges.Weight;
+axis off
 
-colors = jet(info.players);
-
+%% all taken edges
 for edge = 1:numedges(G.taken)
     player = G.taken.Edges.Weight(edge);
     if player ~= 0
         [s, t] = findedge(G.taken, edge);
-        highlight(h, s, t, 'EdgeColor', colors(player,:), 'LineWidth', 5)
+        highlight(h, s, t, 'EdgeColor', playercolors(player,:), 'LineWidth', 5)
     end
 end
 
-% this is a slow, stupid way to code this
-% it also doesn't say which goal cities go with which
-% maybe do this based on turn
-for node = 1:numnodes(G.taken)
-    nodename = char(G.distance.Nodes.Name(node));
-    needed = [];
-    for player = 1:info.players
-        for goal = 1:length(cards.playergoals{player})
-            for city = 1:2
-                if strcmp(nodename, cards.playergoals{player}{goal}{city})
-                    needed(end+1) = player;
-                end
-            end
-        end
-    end
-    if ~isempty(needed)
-        if length(needed) > 1 && any(turn == needed)
-            highlight(h, nodename, 'NodeColor', colors(turn,:), 'MarkerSize', 10)
-        else
-            highlight(h, nodename, 'NodeColor', colors(needed(1),:), 'MarkerSize', 10)
-        end
+%% current player's goals
+goalcolors = parula(length(cards.playergoals{turn}));
+for goal = 1:size(goalcolors, 1)
+    for city = 1:2
+        highlight(h, cards.playergoals{turn}{goal}{city}, 'NodeColor', goalcolors(goal,:), 'MarkerSize', 10)
     end
 end
-
 
 %%
 drawnow
