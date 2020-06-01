c9d9465234539bed58e3ae666d929aef4d7ab41d
Elon Gaffin-Cahn <egaffincahn@gmail.com>
2020-06-01 13:19:56 -0400

    add longest road
    -Sped up the function to find longest road
    -Tested the algorithm against several tests

--- code/add_longest_road.py
+++ code/add_longest_road.py
@@ -2,80 +2,145 @@ import networkx as nx
 import copy
 import numpy as np
 
-
+# We take the graph of track distances and then only keep the ones where the current player has laid the track. Then we
+# take the remaining connections (the player's tracks) and build a full adjacency matrix, which is a symmetric matrix
+# (# cities x # cities) where there is a 1 at each (row,column) pair where the two cities are connected. Start with a
+# column that has a connection, and then look down the column for connected cities. If there's only 1, remove that
+# connection, then do the same for the connected city. If there are ever 2 or more cities connected, duplicate
+# everything and keep going down each path.
 def add_longest_road(game):
-    player_dists = np.zeros(game.players, dtype=np.int16)
-    player_paths = [[] for _ in range(game.players)]
 
-    for turn in range(game.players):
+    def compare(old=None, new=None, players=None):
+        if players is not None:  # initialize
+            return dict(turn=np.arange(players), length=0)
+        elif new['length'] > old['length']:
+            return new
+        elif new['length'] == old['length'] and new['turn'] not in old['turn']:
+            return dict(length=old['length'], turn=np.unique(np.concatenate((new['turn'], old['turn']))))
+        else:
+            return old
 
-        H = game.map.create_player_subgraph(turn, multi=True, incl_nodes=True)
-        full_adj_mat = nx.adjacency_matrix(H).todense()  # faster than the other methods of making adj matrix
-        path = []  # list of lists; each item is a route through each node index
+    def by_turn(map, players):
+        best = compare(players=players)
+        for turn in range(players):
+            G = map.create_player_subgraph(turn, multi=False, incl_nodes=True)
+            G.turn = turn
+            groups = find_groups(G)
+            best = by_group(G, groups, best)
+        return best
+
+    def sum_edges(edges):
+        edges = [(e[0], e[1], 0) for e in edges]
+        return sum(game.map.extract_feature(edges, feature='distance'))
+
+    def by_group(G, groups, best):
+        if len(groups) == 0:
+            return best
+        for g, nbunch in enumerate(groups):
+            H = copy.deepcopy(G)
+            H.remove_edges_from([e for e in H.edges if not np.any(np.isin(nbunch, e))])
+            if sum_edges(H.edges) >= best['length']:  # skip if this group cannot be longer
+                best = DFS(H, best)
+        return best
+
+    def find_groups(G):
+        H = copy.deepcopy(G)
+        groups = []
+        for frm, to in dict(nx.shortest_path_length(H)).items():
+            if frm in H.nodes and len(list(H.neighbors(frm))) > 0:
+                grp = list(to.keys())
+                groups.append(grp)
+                H.remove_nodes_from(grp)
+        return groups
+
+    def extend(base, node=None, edge=None):
+        if node is not None and isinstance(node, list) and len(node) == 1:
+            node = node[0]
+        if node is not None:
+            extension = node
+        elif edge is not None:
+            extension = game.map.get_edge_index((game.map.get_node_name(edge[0]), game.map.get_node_name(edge[1]), 0))
+        else:
+            raise self.Error('argument error')
+        return np.int8(np.concatenate((base, [extension])))
+
+    def DFS(G, best):
+
+        full_adj_mat = nx.adjacency_matrix(G).todense()  # faster than the other methods of making adj matrix
+        path_nodes = []  # list of lists; each item is a route through each node index
+        path_edges = []
         terminated = []  # list representing whether the path has been fully searched
         trim_adj_mat = []  # list of adjacency matrices that have edges removed
         path_dists = []  # list for this player where each index is the length of a path, only updated once terminated
-        found_global_best = False  # tells us if there's a path that has used every connection - allows us to quit early
 
-        for n in np.flatnonzero(np.sum(full_adj_mat, axis=0)):  # only look at the cities that have connections
-            path.append([n])  # initialize a new path with the current city we start with
+        seeds = np.flatnonzero(np.array(list(dict(G.degree).values())) % 2 == 1)  # only start with nodes of odd degree
+        if len(seeds) == 0:
+            seeds = np.flatnonzero(np.sum(full_adj_mat, axis=0))  # use even degree nodes if we have to
+
+        if sum(d % 2 == 1 for _, d in G.degree()) in (0, 2):  # check if semi-eulerian
+            new = dict(turn=np.array([G.turn]), length=sum_edges(G.edges))
+            return compare(new=new, old=best)
+
+        for n in seeds:  # n: seed
+            path_nodes.append([n])  # initialize a new path with the current city we start with
+            path_edges.append([])
             terminated.append(False)  # this new path has not been finished
 
             trim_adj_mat.append(copy.deepcopy(full_adj_mat))
 
-            while any(np.logical_not(terminated)) and not found_global_best:
+            while any(np.logical_not(terminated)):
 
-                curr_path_index = np.flatnonzero(np.logical_not(terminated))[0]  # indexes the current index of the path (and others lists)
-                n_ = path[curr_path_index][-1]
+                curr_path_index = np.flatnonzero(np.logical_not(terminated))[0]
+                n_ = path_nodes[curr_path_index][-1]  # n_: current node
                 connected_nodes = np.flatnonzero(trim_adj_mat[curr_path_index][:, n_])
                 num_connected = len(connected_nodes)
 
-                if num_connected == 1:
-                    n__ = connected_nodes
-                    path[curr_path_index] = np.concatenate((path[curr_path_index], n__))  # add the connected to the path vector
+                if num_connected == 0:
+
+                    edges = np.int8(path_edges[curr_path_index])
+                    path_dists.append(sum(game.map.extract_feature(edges, feature='distance')))
+                    terminated[curr_path_index] = True  # don't need to return to this path
+
+                elif num_connected == 1:
+
+                    n__ = connected_nodes[0]  # n__: next node
+                    path_nodes[curr_path_index] = extend(path_nodes[curr_path_index], node=n__)
+                    path_edges[curr_path_index] = extend(path_edges[curr_path_index], edge=(n_, n__))
 
                     # remove the edge
                     trim_adj_mat[curr_path_index][n_, n__] = 0
                     trim_adj_mat[curr_path_index][n__, n_] = 0
+
                 elif num_connected > 1:
-                    # remove the current info (terminated, path, trim_adj_mat) in lieu of the forks
-                    terminated = [t for i, t in enumerate(terminated) if i != curr_path_index]
-                    for n__ in connected_nodes:
+
+                    for n__ in connected_nodes:  # n__: next node
+
                         # create a new path and add the connected city to it
-                        path.append(np.concatenate((path[curr_path_index], [n__])))
+                        path_nodes.append(extend(path_nodes[curr_path_index], node=n__))
+                        path_edges.append(extend(path_edges[curr_path_index], edge=(n_, n__)))
+
                         terminated.append(False)
                         trim_adj_mat.append(copy.deepcopy(trim_adj_mat[curr_path_index]))
 
                         # remove edge
                         trim_adj_mat[-1][n_, n__] = 0
                         trim_adj_mat[-1][n__, n_] = 0
-                    trim_adj_mat = [t for i, t in enumerate(trim_adj_mat) if i != curr_path_index]
-                    path = [p for i, p in enumerate(path) if i != curr_path_index]  # this path is replaced with the forks
-                elif num_connected == 0:  # when there are no more connected cities on this path
-                    edges = []
-                    # gets the edge indices for the cities in the path
-                    for i in range(len(path[curr_path_index]) - 1):
-                        n0 = game.map.get_node_name(path[curr_path_index][i])
-                        n1 = game.map.get_node_name(path[curr_path_index][i + 1])
-                        edges.append(game.map.get_edge_index((n0, n1, 0)))
-                    n0 = game.map.get_node_name(path[curr_path_index][0])
-                    n1 = game.map.get_node_name(path[curr_path_index][-1])
-                    if len(path[curr_path_index]) > 2 and n0 in nx.neighbors(H, n1) and game.map.get_edge_index((n0, n1, 0)) not in edges:
-                        edges.append(game.map.get_edge_index((n0, n1, 0)))
-                    path_dists.append(sum(game.map.extract_feature(edges, feature='distance')))
-                    if np.all(np.logical_not(trim_adj_mat[curr_path_index])):  # all cities have been connected on this path
-                        found_global_best = True
-                    else:
-                        terminated[curr_path_index] = True  # don't need to return to this path
 
-            if found_global_best:
-                break
+                    # remove the current info, replaced by the forks
+                    del terminated[curr_path_index]
+                    del trim_adj_mat[curr_path_index]
+                    del path_nodes[curr_path_index]
+                    del path_edges[curr_path_index]
 
-        if len(path_dists) < 1:
-            path_dists.append(0)
-        else:
-            best = np.argmax(path_dists)
-            player_dists[turn] = path_dists[best]
-            player_paths[turn] = [game.map.get_node_name(n) for n in path[best]]
+        new = dict(length=0)
+        if len(path_nodes) > 0:
+            new = dict(turn=np.array([G.turn]), length=max(path_dists))
+        return compare(new=new, old=best)
 
-    return player_dists, player_paths
+        # end DFS
+
+    best = by_turn(game.map, game.players)
+    points = np.zeros(game.players, dtype=np.int16)
+    points[best['turn']] = 10  # ties go to both players
+    length = best['length']
+    return length, points
--- code/add_longest_road_unittest.py
+++ code/add_longest_road_unittest.py
@@ -7,31 +7,30 @@ from add_longest_road import add_longest_road
 
 num_test = 0
 turn = 0
+turn_ = 1
 pop = Population(players=3)
 
 
-def test(edges, expected, test_num):
+def test(test_num, edges, expected_length, expected_points=[10, 0, 0], edges_=None):
     game = Game(pop.cohort)
     [game.map.lay_track(turn, game.map.get_edge_index(e)) for e in edges]
+    if edges_ is not None:
+        [game.map.lay_track(turn_, game.map.get_edge_index(e)) for e in edges_]
     tStart = dt.now()
-    length, path = add_longest_road(game)
+    length, points = add_longest_road(game)
     tFinish = dt.now()
-    if not isinstance(expected, list):
-        expected = [expected, 0, 0]
-    else:
-        expected.append(0)
-    if any(np.not_equal(length, expected)):
+    if length != expected_length or np.any(np.not_equal(points, expected_points)):
         game.map.plot_graph(turn)
-        raise Exception('failed test {}: expected {}, counted {}\n{}'.format(test_num, expected, length, path))
+        raise Exception('failed test {}:\nexpected len {}, counted {}\nexpected pts {}, counted {}'.format(test_num, expected_length, length, expected_points, points))
     else:
-        print('passed test {}'.format(test_num))
+        print('passed test {}: {}'.format(test_num, tFinish - tStart))
     return tFinish - tStart
 
 
 # single track
 num_test += 1
 edges = [('Vancouver', 'Calgary', 0)]  # +3
-test(edges, 3, num_test)
+test(num_test, edges, 3)
 
 # string of tracks
 num_test += 1
@@ -40,7 +39,7 @@ edges = [
     ('Calgary', 'Winnipeg', 0),  # +6
     ('Winnipeg', 'Duluth', 0)  # +4
 ]
-test(edges, 14, num_test)
+test(num_test, edges, 14)
 
 # loop
 num_test += 1
@@ -49,7 +48,7 @@ edges = [
     ('Montreal', 'New York', 0),  # +3
     ('New York', 'Boston', 0)  # +2
 ]
-test(edges, 7, num_test)
+test(num_test, edges, 7)
 
 # loop + tail
 num_test += 1
@@ -60,7 +59,7 @@ edges = [
     ('Sault St. Marie', 'Montreal', 0),  # +5
     ('Sault St. Marie', 'Duluth', 0),  # +3
 ]
-test(edges, 15, num_test)
+test(num_test, edges, 15)
 
 # multiple unconnected subgraphs
 num_test += 1
@@ -75,7 +74,28 @@ edges = [
     ('Montreal', 'Boston', 0),  # 2
     ('Washington', 'New York', 0)  # 2
 ]
-test(edges, 25, num_test)
+test(num_test, edges, 25)
+
+# one winner
+num_test += 1
+edges = [
+    ('Seattle', 'Vancouver', 0)  # 1
+]
+edges_ = [
+    ('Calgary', 'Helena', 0)  # +4
+]
+test(num_test, edges, 4, expected_points=[0, 10, 0], edges_=edges_)
+
+# tie
+num_test += 1
+edges = [
+    ('Calgary', 'Helena', 0)  # +4
+]
+edges_ = [
+    ('Denver', 'Kansas City', 0)  # +4
+]
+test(num_test, edges, 4, expected_points=[10, 10, 0], edges_=edges_)
+
 
 # 1-1 fork
 num_test += 1
@@ -84,7 +104,7 @@ edges = [
     ('Helena', 'Duluth', 0),  # +6
     ('Helena', 'Denver', 0)  # (+)4
 ]
-test(edges, 10, num_test)
+test(num_test, edges, 10)
 
 # 2-1 fork
 num_test += 1
@@ -94,7 +114,7 @@ edges = [
     ('Helena', 'Denver', 0),  # +4
     ('Denver', 'Omaha', 0)  # +4
 ]
-test(edges, 14, num_test)
+test(num_test, edges, 14)
 
 # three forks
 num_test += 1
@@ -106,7 +126,7 @@ edges = [
     ('Seattle', 'Calgary', 0),  # +4
     ('San Francisco', 'Salt Lake City', 0)  # +5
 ]
-test(edges, 15, num_test)
+test(num_test, edges, 15)
 
 # non-included fork off loop
 num_test += 1
@@ -119,7 +139,7 @@ edges = [
     ('Seattle', 'Calgary', 0),  # +4
     ('San Francisco', 'Salt Lake City', 0)  # +5
 ]
-test(edges, 25, num_test)
+test(num_test, edges, 25)
 
 # non-included loop off fork
 num_test += 1
@@ -133,7 +153,7 @@ edges = [
     ('Seattle', 'Calgary', 0),  # +4
     ('San Francisco', 'Salt Lake City', 0)  # +5
 ]
-test(edges, 25, num_test)
+test(num_test, edges, 25)
 
 # double loop
 num_test += 1
@@ -150,7 +170,7 @@ edges = [
     ('Saint Louis', 'Pittsburgh', 0),  # +5
     ('Pittsburgh', 'Nashville', 0)  # +4
 ]
-test(edges, 28, num_test)
+test(num_test, edges, 28)
 
 # double loop with fork
 num_test += 1
@@ -168,7 +188,7 @@ edges = [
     ('Pittsburgh', 'Nashville', 0),  # +4
     ('Kansas City', 'Omaha', 0)  # 1
 ]
-test(edges, 28, num_test)
+test(num_test, edges, 28)
 
 # fork with double loop, one loop unused
 num_test += 1
@@ -186,7 +206,7 @@ edges = [
     ('Pittsburgh', 'Nashville', 0),  # +4
     ('New Orleans', 'Miami', 0)  # +6
 ]
-test(edges, 31, num_test)
+test(num_test, edges, 31)
 
 # long track with forking loops, forks off loops, double loops
 num_test += 1
@@ -198,7 +218,7 @@ edges = [('Seattle', 'Portland', 0), ('Portland', 'Salt Lake City', 0), ('Salt L
          ('Omaha', 'Chicago', 0), ('Chicago', 'Pittsburgh', 0), ('Pittsburgh', 'Nashville', 0),
          ('Nashville', 'Little Rock', 0), ('Little Rock', 'Dallas', 0), ('Houston', 'New Orleans', 0),
          ('New Orleans', 'Atlanta', 0), ('Nashville', 'Atlanta', 0), ('Atlanta', 'Miami', 0)]
-test(edges, 59, num_test)
+test(num_test, edges, 59)
 
 # long track with forking loops, forks off loops, double loops, connected
 num_test += 1
@@ -211,7 +231,7 @@ edges = [('Seattle', 'Portland', 0), ('Portland', 'Salt Lake City', 0), ('Salt L
          ('Nashville', 'Little Rock', 0), ('Little Rock', 'Dallas', 0), ('Houston', 'New Orleans', 0),
          ('New Orleans', 'Atlanta', 0), ('Nashville', 'Atlanta', 0), ('Atlanta', 'Miami', 0),
          ('Salt Lake City', 'Helena', 0), ('Helena', 'Omaha', 0)]
-test(edges, 67, num_test)
+test(num_test, edges, 67)
 
 
 print('passed all tests')
